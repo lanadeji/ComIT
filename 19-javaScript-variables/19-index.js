9f66a7187f6ff297c8067649597883ff82791644
lanadeji <dejiolulana@gmail.com>
2020-02-28 08:15:40 -0700

    19-javaScript-variables

---
+++ 19-javaScript-variables/19-index.js
@@ -0,0 +1,22 @@
+const monthOfBirth = "August";
+const firstName = "Barack";
+const lastName = "Obama";
+
+let city;
+let age;
+let nextCity;
+
+city = "Honolulu";
+age = 58;
+
+nextCity = city;
+
+console.log("monthOfBirth: ", monthOfBirth);
+console.log("firstName: ", firstName);
+console.log("lastName: ", lastName);
+console.log("city: ", city);
+console.log("age: ", age);
+console.log("nextCity: ", nextCity);
+
+console.log(firstName, lastName);
+
