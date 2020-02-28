af40fb40803890d6827811114fefa970673de344
lanadeji <dejiolulana@gmail.com>
2020-02-28 08:11:03 -0700

    19-javaScript-variables

---
+++ 20-javaScript-conversions/20-index.js
@@ -0,0 +1,25 @@
+let age = 35;
+let firstName = "Jason";
+let completedCourse = false;
+let finalScore = null;
+let winningNumbers;
+
+age = true;
+firstName = null;
+completedCourse = 17;
+finalScore = "Winner!";
+winningNumbers = 349;
+
+age = null;
+firstName = true;
+completedCourse = "Don't ask me!";
+finalScore = 69;
+winningNumbers = undefined;
+
+
+
+console.log("age: ", age);
+console.log("firstName: ", firstName);
+console.log(completedCourse);
+console.log(finalScore);
+console.log(winningNumbers);
