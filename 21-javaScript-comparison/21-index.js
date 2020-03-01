a13d46242087af38eb2cded7fd902c095a8bd8aa
lanadeji <dejiolulana@gmail.com>
2020-02-29 22:57:56 -0700

    21-javaScript-comparison

---
+++ 21-javaScript-comparison/21-index.js
@@ -0,0 +1,11 @@
+alert("Hello there!"); // Welcome greeting
+
+alert(2 == 1);
+
+age = prompt("How old are you?");
+console.log("age:", age);
+
+height = prompt("How tall are you?");
+console.log("height:", height);
+
+confirm(age > height);
